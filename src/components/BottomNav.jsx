ac4a53192d5910bd57d6469e413db7837c82331c
threelokin <threelok.in@gmail.com>
2024-10-01 12:59:19 +0530

    Update BottomNav.jsx

--- src/components/BottomNav.jsx
+++ src/components/BottomNav.jsx
@@ -3,9 +3,9 @@ import { NavLink } from 'react-router-dom';
 import { GoHomeFill } from "react-icons/go";
 import { RiRobot2Fill, RiSettings4Fill, RiSearch2Fill } from "react-icons/ri";
 
-const BottomNav = () => {
+const BottomNav = ({ visible }) => {
   return (
-    <nav className="bg-white p-4 h-16 flex justify-around items-center fixed bottom-0 w-full shadow-[0_35px_60px_15px_rgba(0,0,0,0.5)]">
+    <nav className={`bg-white p-4 h-16 flex justify-around items-center fixed bottom-0 w-full shadow-[0_35px_60px_15px_rgba(0,0,0,0.5)] transition-transform duration-300 ease-in-out ${visible ? 'translate-y-0' : 'translate-y-full'}`}>
       <NavLink
         to="/"
         className={({ isActive }) => `nav-link ${isActive ? 'text-blue-700 text-3xl' : 'text-gray-500 text-xl'}`}
