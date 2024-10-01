e4a83a3ac552aff137181598432844c1801336b2
threelokin <threelok.in@gmail.com>
2024-10-01 13:40:25 +0530

    Update TopNav.jsx

--- src/components/TopNav.jsx
+++ src/components/TopNav.jsx
@@ -37,41 +37,45 @@ const TopNav = ({ visible, onLanguageSelect }) => {
   };
 
   return (
-    <div className={`fixed top-0 left-0 w-full bg-white shadow-md z-40 h-14 transition-transform duration-300 ease-in-out ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
-      <nav className="flex items-center justify-between">
-        <div className="flex items-center">
-          <span>
-            <div className='-mt-3'>
-              <img src={logo} alt="3Lok News" className='h-20' />
-            </div>
-          </span>
-        </div>
-        <div className="flex items-center">
-          {!menuOpen && (
-            <button onClick={toggleMenu} className="text-black-900 text-2xl mr-3 focus:outline-none -mt-3">
-              <HiMenuAlt3 />
-            </button>
-          )}
-          {menuOpen && (
-            <button onClick={closeMenu} className="text-2xl absolute right-4 top-1/2 transform -translate-y-1/2 z-50">
-              <IoClose />
-            </button>
-          )}
-        </div>
-      </nav>
-      <div className={`fixed inset-0 bg-white z-30 transition-transform duration-300 ease-in-out ${menuOpen ? 'translate-x-0' : 'translate-x-full'}`}>
-        <div className="text-black text-center flex items-center justify-center h-full">
-          <ul className="space-y-4">
-            <li>
-              <Link to="/" onClick={closeMenu} className="text-2xl">{navItems[language].home}</Link>
-            </li>
-            <li>
-              <Link to="/about" onClick={closeMenu} className="text-2xl">{navItems[language].about}</Link>
-            </li>
-          </ul>
-        </div>
+    <>
+      <div className={`fixed top-0 left-0 w-full bg-white shadow-md z-40 h-14 transition-transform duration-300 ease-in-out ${visible ? 'translate-y-0' : '-translate-y-full'}`}>
+        <nav className="flex items-center justify-between">
+          <div className="flex items-center">
+            <span>
+              <div className='-mt-3'>
+                <img src={logo} alt="3Lok News" className='h-20' />
+              </div>
+            </span>
+          </div>
+          <div className="flex items-center">
+            {!menuOpen && (
+              <button onClick={toggleMenu} className="text-black-900 text-2xl mr-3 focus:outline-none -mt-3">
+                <HiMenuAlt3 />
+              </button>
+            )}
+            {menuOpen && (
+              <button onClick={closeMenu} className="text-2xl absolute right-4 top-1/2 transform -translate-y-1/2 z-50">
+                <IoClose />
+              </button>
+            )}
+          </div>
+        </nav>
       </div>
-    </div>
+      {menuOpen && (
+        <div className="fixed inset-0 bg-white z-30 transition-transform duration-300 ease-in-out">
+          <div className="text-black text-center flex items-center justify-center h-full">
+            <ul className="space-y-4">
+              <li>
+                <Link to="/" onClick={closeMenu} className="text-2xl">{navItems[language].home}</Link>
+              </li>
+              <li>
+                <Link to="/about" onClick={closeMenu} className="text-2xl">{navItems[language].about}</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
